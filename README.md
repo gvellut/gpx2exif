b1451afbf2f1853c0b29471d1f92cd696dd0c6fc
Guilhem Vellut <g@vellut.com>
2020-03-17 14:08:12 +0100

    Finished implementation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gpx2exif
 
-Simple command-line tool to add GPS info from a GPX file to the EXIF tags in images
+Simple command-line tool to add GPS info from a GPX file to EXIF tags in images
 
 # Motivation
 
@@ -18,7 +18,17 @@ pip install gpx2exif
 
 The command above will install the `gpx2exif` Python library and its dependencies. The library includes a command-line script, also named `gpx2exif`, whose functionality is described below.
 
-# Usage
+# Time in images
+
+## Time tag
+
+The time used for the images is taken from the Date Time Original EXIF tag in the image. In Adobe Bridge, it can be shifted as needed in the UI if needed.
+
+## Time zone
+
+There is no standard time zone tag in EXIF. Some cameras will set the Offset Time Original tag to a time shift (something like "+02:00"), which, by default, is read by the tool in order to set a zone. If this tag is not present, the zone of the times in the images is assumed to be UTC. In that case, if the times in the images are actually in local time, the `--delta` switch must be used to compensate. The `--ignore-offset` switch can also be used to make the tool ignore the Offset Time Original tag even if present (for instance, if it is wrong).
+
+For example, if the local time is in the "Europe/Paris" time zone aka GMT+1 during winter, it is equivalent to an Offset Time Original of "+01:00". This means that, if the time in the image is 11:15am in local time, it is 10:15am in UTC. If the Offset Time Original is not present (or is ignored), then the `--delta` switch must be set to `-1h` to compensate: The 11:15am found in the EXIF tag is considered to be in UTC but, actually, in UTC, it should be 10:15am so the delta must be *minus* 1 hour.
 
 ## Options
 
@@ -31,15 +41,20 @@ Options:
   -d, --delta TEXT      Time shift to apply to the photo Date Time Original
                         EXIF tag to match the date in GPX (see documentation
                         for format). Use if there is a drift in the camera
-                        compared to the GPS recorder
+                        compared to the GPS recorder or if an offset is not
+                        present in the EXIF.
 
   -t, --tolerance TEXT  Tolerance if time of photo is not inside the time
-                        bounds of a GPX segment (default: 10s)
+                        range of the GPX track (default: 10s).
+
+  -o, --ignore-offset   Flag to indicate that the OffsetTimeOriginal should
+                        not be used (time of images is assumed UTC). Use
+                        --delta to compensate for both timezone and drift.
 
   -d, --head            Flag to indicate if the tool should just output the
                         times of the first 10 track points in the GPX and the
                         DateTimeOriginal tag of the first 10 images (useful
-                        for setting the delta)
+                        for setting the --delta).
 
   --help                Show this message and exit.
 ```
@@ -55,3 +70,11 @@ gpx2exif geopaparazzi_20200315_183754.gpx dsc004239.jpg --delta 2m25s
 ```
 
 After running this command, the photo will be updated with the location of the closest GPX track point.
+
+### Folder
+
+Instead of a single file, it is possible to pass a folder:
+
+```console
+gpx2exif geopaparazzi_20200315_183754.gpx photos --delta 2m25s
+```
--- gpx2exif/gpx2exif.py
+++ gpx2exif/gpx2exif.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta, timezone
 from fractions import Fraction
+import logging
 from pathlib import Path
 import re
 import sys
-import traceback
 
 import click
+import colorama
 import gpxpy
 import gpxpy.gpx
 import pandas as pd
@@ -14,7 +15,9 @@ from termcolor import colored
 
 DEBUG = True
 
-# cf https://gist.github.com/c060604/8a51f8999be12fc2be498e9ca56adc72
+logger = logging.getLogger(__package__)
+
+# code for GPS EXIF https://gist.github.com/c060604/8a51f8999be12fc2be498e9ca56adc72
 
 
 def to_deg(value, loc):
@@ -46,30 +49,30 @@ def change_to_rational(number):
     return (f.numerator, f.denominator)
 
 
-def get_gps_ifd(lat, lng, altitude=None):
+def get_gps_ifd(lat, lon, altitude=None):
     """
-    Adds GPS position as EXIF metadata
+    Returns GPS structure of EXIF metadata
     """
     lat_deg = to_deg(lat, ["S", "N"])
-    lng_deg = to_deg(lng, ["W", "E"])
+    lon_deg = to_deg(lon, ["W", "E"])
 
     exiv_lat = (
         change_to_rational(lat_deg[0]),
         change_to_rational(lat_deg[1]),
         change_to_rational(lat_deg[2]),
     )
-    exiv_lng = (
-        change_to_rational(lng_deg[0]),
-        change_to_rational(lng_deg[1]),
-        change_to_rational(lng_deg[2]),
+    exiv_lon = (
+        change_to_rational(lon_deg[0]),
+        change_to_rational(lon_deg[1]),
+        change_to_rational(lon_deg[2]),
     )
 
     gps_ifd = {
         piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
         piexif.GPSIFD.GPSLatitudeRef: lat_deg[3],
         piexif.GPSIFD.GPSLatitude: exiv_lat,
-        piexif.GPSIFD.GPSLongitudeRef: lng_deg[3],
-        piexif.GPSIFD.GPSLongitude: exiv_lng,
+        piexif.GPSIFD.GPSLongitudeRef: lon_deg[3],
+        piexif.GPSIFD.GPSLongitude: exiv_lon,
     }
 
     if altitude is not None:
@@ -83,34 +86,148 @@ def get_gps_ifd(lat, lng, altitude=None):
     return gps_ifd
 
 
-def save_exif_with_gps(file_name, exif_data, gps_ifd):
+def save_exif_with_gps(file_path_s, exif_data, gps_ifd):
     exif_data.update({"GPS": gps_ifd})
     exif_bytes = piexif.dump(exif_data)
-    piexif.insert(exif_bytes, file_name)
+    piexif.insert(exif_bytes, file_path_s)
 
 
-def read_original_photo_time(exif_data, tz_warning=True):
+def compute_pos(img_time, gpx_segments, tolerance):
+    img_time = pd.Timestamp(img_time)
+    tolerance = pd.Timedelta(tolerance)
+    for df in gpx_segments:
+        if img_time in df.index:
+            gps = df.loc[img_time]
+            return gps["lat"], gps["lon"]
+        else:
+            # searchsorted returns the index for insertion to keep the
+            # series sorted with the arg value inserted
+            index = df.index.searchsorted(img_time)
+            if index == 0:
+                # before first
+                dt = df.index[0].tz_convert("utc") - img_time.tz_convert("utc")
+                if dt < tolerance:
+                    # consider the last row as the value
+                    gps = df.iloc[0]
+                    return gps["lat"], gps["lon"]
+                else:
+                    # no suitable point in GPX found
+                    return None
+            elif index == len(df):
+                # after last
+                dt = img_time.tz_convert("utc") - df.index[-1].tz_convert("utc")
+                if dt < tolerance:
+                    # consider the last row as the value
+                    # TODO search the next segment to see if closer ?
+                    gps = df.iloc[-1]
+                    return gps["lat"], gps["lon"]
+                else:
+                    # search next segment
+                    continue
+            else:
+                gps_before = df.iloc[index - 1]
+                gps_after = df.iloc[index]
+                gpx_gap = gps_after.name - gps_before.name
+                img_gap = img_time.tz_convert("utc") - gps_before.name.tz_convert("utc")
+                gap_ratio = img_gap / gpx_gap
+                # linear interp
+                lat = (
+                    gps_before["lat"]
+                    + (gps_after["lat"] - gps_before["lat"]) * gap_ratio
+                )
+                lon = (
+                    gps_before["lon"]
+                    + (gps_after["lon"] - gps_before["lon"]) * gap_ratio
+                )
+                return lat, lon
+
+    return None
+
+
+def process_head(
+    img_fileordirpath, gpx_segments, delta, is_ignore_offset, head_limit=10
+):
+    counter = 0
+    for df in gpx_segments:
+        for value in df.index:
+            logger.info(f"{counter + 1:02d} - GPX Time: {value}")
+            counter += 1
+            if counter == head_limit:
+                break
+        else:
+            continue
+        break
+
+    if img_fileordirpath.is_file():
+        head_image(1, img_fileordirpath, delta, is_ignore_offset)
+    elif img_fileordirpath.is_dir():
+        counter = 0
+        tz_warning = True
+        for img_filepath in sorted(img_fileordirpath.iterdir()):
+            try:
+                head_image(
+                    counter + 1, img_filepath, delta, is_ignore_offset, tz_warning
+                )
+                tz_warning = False
+                counter += 1
+                if counter == head_limit:
+                    break
+            except piexif.InvalidImageDataError:
+                pass
+
+
+def head_image(index, img_path, delta, is_ignore_offset, tz_warning=True):
+    exif_data = piexif.load(str(img_path.resolve()))
+    time_original = read_original_photo_time(exif_data, is_ignore_offset, tz_warning)
+    time_corrected = time_original + delta
+
+    logger.info(
+        f"{index:02d} - Date Time Original (corrected): {time_corrected.isoformat()} "
+        f"[{img_path.name}]"
+    )
+
+
+def process_image(
+    img_path, gpx_segments, delta, tolerance, is_ignore_offset, tz_warning=True
+):
+    exif_data = piexif.load(str(img_path.resolve()))
+    time_original = read_original_photo_time(exif_data, is_ignore_offset, tz_warning)
+    time_corrected = time_original + delta
+
+    logger.debug(f"Time corrected {time_corrected.isoformat()}")
+
+    pos = compute_pos(time_corrected, gpx_segments, tolerance)
+    if not pos:
+        logger.warning(
+            f"Cannot compute position for file {img_path.name} ({time_corrected} "
+            f"is outside GPX range + tolerance)"
+        )
+        return
+
+    lat, lon = pos
+
+    logger.debug(f"Lat / Lon {lat} {lon}")
+    gps_ifd = get_gps_ifd(lat, lon)
+    save_exif_with_gps(str(img_path.resolve()), exif_data, gps_ifd)
+    return
+
+
+def read_original_photo_time(exif_data, is_ignore_offset, tz_warning=True):
     dt_original = exif_data["Exif"][piexif.ExifIFD.DateTimeOriginal]
     dt_original = dt_original.decode("ascii")
     dt_format = "%Y:%m:%d %H:%M:%S"
-    if piexif.ExifIFD.OffsetTimeOriginal in exif_data["Exif"]:
+    if not is_ignore_offset and piexif.ExifIFD.OffsetTimeOriginal in exif_data["Exif"]:
         offset_original = exif_data["Exif"][piexif.ExifIFD.OffsetTimeOriginal]
         offset_original = offset_original.decode("ascii")
         if tz_warning:
-            print(colored(f"Found offset in EXIF: {offset_original}", "yellow"))
-            tz_warning = True
+            logger.warning(f"Found offset in EXIF: {offset_original}")
         # append the offset
         dt_original += offset_original
         dt_format += "%z"
         dt_original = datetime.strptime(dt_original, dt_format)
     else:
         if tz_warning:
-            print(
-                colored(
-                    "No offset in EXIF. Assume UTC. Use --delta to adjust", "yellow"
-                )
-            )
-            tz_warning = True
+            logger.warning("No offset in EXIF. Assume UTC (+00:00)")
         dt_original = datetime.strptime(dt_original, dt_format)
         # assume UTC
         dt_original = dt_original.replace(tzinfo=timezone.utc)
@@ -118,23 +235,23 @@ def read_original_photo_time(exif_data, tz_warning=True):
     return dt_original
 
 
-def parse_gpx(gpx_filepath):
+def read_gpx(gpx_filepath):
     with open(gpx_filepath, "r") as gpx_file:
         gpx = gpxpy.parse(gpx_file)
 
     df_segments = []
     for track in gpx.tracks:
         for segment in track.segments:
-            lats, lngs, times = [], [], []
+            lats, lons, times = [], [], []
             for pt in segment.points:
+                # TODO get elevation ?
                 lats.append(pt.latitude)
-                lngs.append(pt.longitude)
+                lons.append(pt.longitude)
                 times.append(pt.time)
-            d = {"lat": lats, "lng": lngs, "time": times}
+            d = {"lat": lats, "lon": lons, "time": times}
             df = pd.DataFrame(d)
             df = df.set_index("time")
             df_segments.append(df)
-            print(df.head())
 
     return df_segments
 
@@ -165,6 +282,35 @@ def parse_timedelta(time_str):
     return timedelta(**time_params)
 
 
+# specify colors for different logging levels
+LOG_COLORS = {logging.ERROR: colorama.Fore.RED, logging.WARNING: colorama.Fore.YELLOW}
+
+
+class ColorFormatter(logging.Formatter):
+    def format(self, record, *args, **kwargs):
+        if record.levelno in LOG_COLORS:
+            record.msg = "{color_begin}{message}{color_end}".format(
+                message=record.msg,
+                color_begin=LOG_COLORS[record.levelno],
+                color_end=colorama.Style.RESET_ALL,
+            )
+        return super(ColorFormatter, self).format(record, *args, **kwargs)
+
+
+def setup_logging():
+    global logger
+    if DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = ColorFormatter("%(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
 @click.command()
 @click.argument(
     "gpx_filepath",
@@ -172,7 +318,7 @@ def parse_timedelta(time_str):
     type=click.Path(exists=True, resolve_path=True, dir_okay=False),
 )
 @click.argument(
-    "img_filepath",
+    "img_fileordirpath",
     metavar="IMAGE_FILE_OR_DIR",
     type=click.Path(exists=True, resolve_path=True),
 )
@@ -183,8 +329,8 @@ def parse_timedelta(time_str):
     help=(
         "Time shift to apply to the photo Date Time Original EXIF tag "
         "to match the date in GPX (see documentation for format). Use if there is a "
-        "drift in the camera compared to the GPS recorder or if a timezone offset "
-        "is not set in the EXIF"
+        "drift in the camera compared to the GPS recorder or if an offset "
+        "is not present in the EXIF."
     ),
     required=False,
 )
@@ -193,11 +339,23 @@ def parse_timedelta(time_str):
     "--tolerance",
     "tolerance",
     help=(
-        "Tolerance if time of photo is not inside the time bounds of a GPX segment "
-        "(default: 10s)"
+        "Tolerance if time of photo is not inside the time range of the GPX track "
+        "(default: 10s)."
     ),
     required=False,
     default="10s",
+)
+@click.option(
+    "-o",
+    "--ignore-offset",
+    "is_ignore_offset",
+    is_flag=True,
+    help=(
+        "Flag to indicate that the OffsetTimeOriginal should not be used (time of "
+        "images is assumed UTC). Use --delta to compensate for both timezone and "
+        "drift."
+    ),
+    required=False,
 )
 @click.option(
     "-d",
@@ -207,60 +365,77 @@ def parse_timedelta(time_str):
     help=(
         "Flag to indicate if the tool should just output the times of the first "
         "10 track points in the GPX and the DateTimeOriginal tag of the first "
-        "10 images (useful for setting the delta)"
+        "10 images (useful for setting the --delta)."
     ),
     required=False,
 )
-def gpx2exif(gpx_filepath, img_filepath, delta, tolerance, is_head):
+def gpx2exif(
+    gpx_filepath, img_fileordirpath, delta, tolerance, is_ignore_offset, is_head
+):
     if delta:
+        logger.info("Parsing time shift...")
         delta = parse_timedelta(delta)
     else:
-        # zero
         delta = timedelta(0)
+    logger.info(colored(f"Time shift: {int(delta.total_seconds())}s", "green"))
 
-    df_segments = parse_gpx(gpx_filepath)
+    if tolerance:
+        logger.info("Parsing tolerance...")
+        # in case negative
+        tolerance = timedelta(seconds=abs(parse_timedelta(tolerance).total_seconds()))
+    else:
+        tolerance = timedelta(seconds=10)
+    logger.info(colored(f"Tolerance: {int(tolerance.total_seconds())}s", "green"))
+
+    logger.info("Parsing GPX...")
+    gpx_segments = read_gpx(gpx_filepath)
+    logger.info(
+        f"GPX time range: {gpx_segments[0].iloc[0].name} => "
+        f"{gpx_segments[-1].iloc[0-1].name}"
+    )
+
+    img_fileordirpath = Path(img_fileordirpath)
 
     if is_head:
-        pass
+        logger.info("Head...")
+        process_head(img_fileordirpath, gpx_segments, delta, is_ignore_offset)
+        return
 
-    img_path = Path(img_filepath)
-
-    if img_path.is_file():
-        img_path = str(img_path.resolve())
-        exif_data = piexif.load(img_path)
-        time_original = read_original_photo_time(exif_data)
-        time_corrected = time_original + delta
-        # TODO process multiple
-        df = df_segments[0]
-        image_time = pd.Timestamp(time_corrected)
-        index = df.index.searchsorted(image_time)
-        if index == 0:
-            # before first
-            pass
-        elif index == len(df):
-            # after last
-            pass
-        else:
-            # searchsorted returns the index of insertion
-            # TODO check if actually present in the index
-            # TODO linear interp between before and after if needed
-            gps = df.iloc[index - 1]
-            print(f"{gps['lat']} {gps['lng']} {gps.name}")
-            gps_ifd = get_gps_ifd(gps["lat"], gps["lng"])
-            save_exif_with_gps(img_path, exif_data, gps_ifd)
-
-    elif img_path.is_dir():
-        pass
+    logger.info("Synching EXIF GPS to GPX...")
+    if img_fileordirpath.is_file():
+        process_image(
+            img_fileordirpath, gpx_segments, delta, tolerance, is_ignore_offset
+        )
+    elif img_fileordirpath.is_dir():
+        tz_warning = True
+        for img_filepath in sorted(img_fileordirpath.iterdir()):
+            # do not process hidden files (sometimes used by the OS to store
+            # metadata, like .DS_store on macOS)
+            if img_filepath.is_file() and not img_filepath.name.startswith("."):
+                try:
+                    process_image(
+                        img_filepath,
+                        gpx_segments,
+                        delta,
+                        tolerance,
+                        is_ignore_offset,
+                        tz_warning,
+                    )
+                    tz_warning = False
+                except piexif.InvalidImageDataError:
+                    logger.error(
+                        f"File {img_filepath.name} is not a JPEG or TIFF image"
+                    )
 
 
 def main():
+    setup_logging()
     try:
         gpx2exif()
     except Exception as ex:
-        print(colored("*** An unrecoverable error occured ***", "red"))
-        print(colored(str(ex), "red"))
-        if DEBUG:
-            traceback.print_exc()
+        logger.error("*** An unrecoverable error occured ***")
+        lf = logger.error if not DEBUG else logger.exception
+        lf(str(ex))
         sys.exit(1)
 
 
