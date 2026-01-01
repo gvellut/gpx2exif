78482c3a2bd6d1718026cb93d322271b76c162c8
Guilhem Vellut <guilhem.vellut@gmail.com>
2026-01-01 17:03:17 +0100

    Changes

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,12 +1,12 @@
 export PYTHONPATH=`pwd`/..
 
 top_photo_folder="/Volumes/CrucialX8/photos"
-folder="20251223_sauvage_biolle"
-gpx="/Users/guilhem/Library/CloudStorage/GoogleDrive-guilhem.vellut@gmail.com/My Drive/___gpx/20251223-090133.gpx"
+folder="20251226_semnoz"
+gpx="/Users/guilhem/Library/CloudStorage/GoogleDrive-guilhem.vellut@gmail.com/My Drive/___gpx/20251226-123041.gpx"
 
-d_tz95="-1h16m50s"
+d_tz95="-1h16m54s"
 d_rx100="16:18:56-17:15:03"
-d_xs20="-59m5s"
+d_xs20="-59m3s"
 
 f_tz95=1
 f_rx100=0
